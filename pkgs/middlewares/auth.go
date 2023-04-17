f307133ff5afcba4956534635cb11e2f3e68c0a3
sosolyht <sosolyht@gmail.com>
2023-04-17 12:16:38 +0900

    refactor: auth 관련

--- pkgs/middlewares/auth.go
+++ pkgs/middlewares/auth.go
@@ -12,12 +12,12 @@ import (
 
 func getToken(ctx *fiber.Ctx) (tokenString string, err error) {
 	reqToken := ctx.Get("Authorization")
-	if !strings.HasPrefix(reqToken, "Bearer") {
+	if !strings.HasPrefix(reqToken, "Bearer ") {
 		err = response.ErrUnauthorized
-		return "", response.ErrorResponse(ctx, err, nil)
+		return "", err
 	}
 
-	tokenString = strings.TrimPrefix(reqToken, "Bearer ")
+	tokenString = strings.TrimSpace(strings.TrimPrefix(reqToken, "Bearer"))
 
 	return
 }
@@ -26,12 +26,16 @@ func parseToken(tokenString string) (*jwt.Token, error) {
 	cfg := cmd.LoadConfig()
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, errors.New("wrong signing method")
+			return nil, errors.New("unsupported signing method")
 		}
 		return []byte(cfg.SigningSecret), nil
 	})
 	if err != nil {
-		return nil, errors.New("bad jwt token")
+		return nil, errors.New("invalid JWT token")
+	}
+
+	if !token.Valid {
+		return nil, errors.New("invalid JWT token")
 	}
 
 	return token, nil
@@ -41,20 +45,17 @@ func AuthRequired() func(*fiber.Ctx) error {
 	return func(ctx *fiber.Ctx) error {
 		tokenString, err := getToken(ctx)
 		if err != nil {
-			err = response.ErrUnauthorized
-			return response.ErrorResponse(ctx, err, nil)
+			return response.ErrorResponse(ctx, response.ErrUnauthorized, nil)
 		}
 
 		token, err := parseToken(tokenString)
 		if err != nil {
-			err = response.ErrUnauthorized
-			return response.ErrorResponse(ctx, err, nil)
+			return response.ErrorResponse(ctx, response.ErrUnauthorized, nil)
 		}
 
 		_, ok := token.Claims.(jwt.MapClaims)
 		if !ok {
-			err = response.ErrUnauthorized
-			return response.ErrorResponse(ctx, err, nil)
+			return response.ErrorResponse(ctx, response.ErrUnauthorized, nil)
 		}
 
 		return ctx.Next()
@@ -80,8 +81,16 @@ func ExtractUserId(ctx *fiber.Ctx) (uuid.UUID, error) {
 		return uuid.Nil, response.ErrorResponse(ctx, err, nil)
 	}
 
-	userId, _ := uuid.Parse(claims["user_id"].(string))
+	userId, ok := claims["user_id"].(string)
+	if !ok {
+		return uuid.Nil, response.ErrorResponse(ctx, response.ErrUnauthorized, nil)
+	}
 
-	return userId, err
+	parsedUserId, err := uuid.Parse(userId)
+	if err != nil {
+		return uuid.Nil, response.ErrorResponse(ctx, response.ErrUnauthorized, nil)
+	}
+
+	return parsedUserId, err
 
 }
